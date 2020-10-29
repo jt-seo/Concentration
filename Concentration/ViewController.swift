8e8a780dc98b77b7364406fc15b0caa3d022a16e
jt.seo <jt.seo@nexplayer.com>
2020-10-29 15:34:08 +0900

    Initial Commit

---
+++ Concentration/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Concentration
+//
+//  Created by JT3 on 2020/10/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
